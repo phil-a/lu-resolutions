df1c7275dd6617a6a3e94db5b9b7f9d4c0c7f680
phil-a <phil.alekseev@gmail.com>
2015-12-31 00:47:56 -0500

    add temporary session data

--- resolutions.js
+++ resolutions.js
@@ -3,7 +3,14 @@ Resolutions = new Mongo.Collection('resolutions');
 if (Meteor.isClient) {
   Template.body.helpers({
     resolutions: function () {
-      return Resolutions.find();
+      if (Session.get('hideFinished')) {
+        return Resolutions.find({checked: {$ne: true}});
+      } else {
+        return Resolutions.find();
+      }
+    },
+    hideFinished :function() {
+      return Session.get('hideFinished');
     }
   });
 
@@ -18,6 +25,10 @@ if (Meteor.isClient) {
 
       event.target.title.value = "";  //clear form
       return false;                   //make sure page doesn't refresh
+    },
+
+    'change .hide-finished': function(event) {
+      Session.set('hideFinished', event.target.checked);
     }
   });
 
