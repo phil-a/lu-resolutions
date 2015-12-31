2bdd749618f12f635199480e15254862610a2f8b
phil-a <phil.alekseev@gmail.com>
2015-12-31 01:18:06 -0500

    add user accounts

--- resolutions.js
+++ resolutions.js
@@ -41,6 +41,11 @@ if (Meteor.isClient) {
       Resolutions.remove(this._id);
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
 
 if (Meteor.isServer) {
