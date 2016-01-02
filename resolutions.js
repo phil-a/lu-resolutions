12e85a4ff6f700d15092e271502584f379906b24
phil-a <phil.alekseev@gmail.com>
2016-01-01 22:54:23 -0500

    add owner to resolutions

--- resolutions.js
+++ resolutions.js
@@ -1,7 +1,6 @@
 Resolutions = new Mongo.Collection('resolutions');
 
 if (Meteor.isClient) {
-
   Meteor.subscribe("resolutions");
 
   Template.body.helpers({
@@ -32,6 +31,12 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.resolution.helpers({
+    isOwner: function(){
+      return this.owner === Meteor.userId();
+    }
+  });
+
   Template.resolution.events({
     'click .toggle-checked': function() {
       Meteor.call("updateResolution", this._id, !this.checked);
@@ -39,6 +44,10 @@ if (Meteor.isClient) {
 
     'click .delete': function(){
       Meteor.call("deleteResolution", this._id);
+    },
+
+    'click .toggle-private': function(){
+      Meteor.call("setPrivate", this._id, !this.private);
     }
   });
 
@@ -62,7 +71,8 @@ Meteor.methods({
   addResolution: function(title) {
     Resolutions.insert({
       title: title,
-      createdAt: new Date()
+      createdAt: new Date(),
+      owner: Meteor.userId()
     });
   },
   updateResolution: function(id, checked) {
@@ -70,5 +80,12 @@ Meteor.methods({
   },
   deleteResolution: function(id) {
     Resolutions.remove(id);
+  },
+  setPrivate: function(id, private) {
+    var res = Resolutions.findOne(id);
+    if(res.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Resolutions.update(id, {$set: {private: private} });
   }
 });
